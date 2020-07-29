a81c46caa879d62e8427afa81ea67956531c42e9
Cristian Silva <cristian123105@gmail.com>
2020-07-29 15:20:14 -0300

    refactor: return new promise on download

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@ import { downloadPDF, readerPDF } from './helpers';
 import Questions from './questions';
 
 const name = Questions();
-console.log(name);
 
-downloadPDF(name);
-readerPDF(name);
+downloadPDF(name)
+  .then(() => {
+    readerPDF(name);
+  })
+  .catch(() => {
+    console.log('deu ruiim');
+  });
--- src/helpers/download.js
+++ src/helpers/download.js
@@ -4,26 +4,29 @@ import ora from 'ora';
 const spinner = ora('Baixando o pdf');
 const downloadPDF = (name) => {
   try {
-    console.log('Iniciando download...');
+    return new Promise((resolve) => {
+      console.log('Iniciando download...');
 
-    const pdf =
-      'http://www.ans.gov.br/images/stories/Plano_de_saude_e_Operadoras/tiss/Padrao_tiss/tiss3/Padrao_TISS_Componente_Organizacional_201704.pdf';
+      const pdf =
+        'http://www.ans.gov.br/images/stories/Plano_de_saude_e_Operadoras/tiss/Padrao_tiss/tiss3/Padrao_TISS_Componente_Organizacional_201704.pdf';
 
-    const options = {
-      directory: './pdf',
-      filename: `Teste_Intuitive_Care_${name}.pdf`,
-    };
+      const options = {
+        directory: './pdf',
+        filename: `Teste_Intuitive_Care_${name}.pdf`,
+      };
 
-    spinner.start();
+      spinner.start();
 
-    download(pdf, options, (error) => {
-      if (error) {
-        spinner.fail('Houve um erro ao baixar o pdf!');
-        console.log('Erro no download: ', error);
-      }
+      download(pdf, options, (error) => {
+        if (error) {
+          spinner.fail('Houve um erro ao baixar o pdf!');
+          console.log('Erro no download: ', error);
+        }
 
-      spinner.stop();
-      spinner.succeed('Download concluido!');
+        spinner.stop();
+        spinner.succeed('Download concluido!');
+        resolve();
+      });
     });
   } catch (error) {
     throw new Error(`Error: ${error}`);
