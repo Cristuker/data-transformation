c054278aa2da669c53bfe11368feadaf699d0f0f
Cristian Silva <cristian123105@gmail.com>
2020-07-30 18:00:25 -0300

    refactor: transform reader pdf in a promise

--- src/helpers/readerPDF.js
+++ src/helpers/readerPDF.js
@@ -1,17 +1,22 @@
-import { resolve } from 'path';
+import { resolve as resolvePath } from 'path';
 import { readFileSync } from 'fs';
 import PDFParser from 'pdf-parse';
 import findText from './findText';
 
 const readerPDF = async (name) => {
-  const filenameAndPath = resolve('pdf', `Teste_Intuitive_Care_${name}.pdf`);
-  const buffer = readFileSync(filenameAndPath);
-  console.log('Arquivo localizado');
-  console.log('Começando a busca pelos quadros...  \n');
-  let dataFinded;
-  PDFParser(buffer).then(async (data) => {
-    dataFinded = await findText(data.text);
-    return dataFinded;
+  return new Promise((resolve) => {
+    const filenameAndPath = resolvePath(
+      'pdf',
+      `Teste_Intuitive_Care_${name}.pdf`
+    );
+    const buffer = readFileSync(filenameAndPath);
+    console.log('Arquivo localizado');
+    console.log('Começando a busca pelos quadros...  \n');
+    let dataFinded;
+    PDFParser(buffer).then(async (data) => {
+      dataFinded = await findText(data.text);
+      return resolve(dataFinded);
+    });
   });
 };
 
